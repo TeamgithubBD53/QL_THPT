d687026e68bd2a1cbfc1d57bf6e20ebc4a7fd555
thangle98 <leanhthang10a1@gmail.com>
2020-12-12 19:52:13 +0700

    p1_CTGD.cs

--- Main/BUS/CTGD.cs
+++ Main/BUS/CTGD.cs
@@ -10,6 +10,35 @@ namespace BUS
 {
     public class CTGD
     {
-
+        public DataTable Show(string tenlop)
+        {
+            string sql = @"select mh.TenMon, gv.HoTen, NgayDay, Tiet 
+                           from tblCTGD ct, tblMonHoc mh, tblLop l, tblGiaoVien gv 
+                            where ct.MaLop=l.MaLop and gv.MaMon=mh.MaMon and ct.MaGV=gv.MaGV and l.tenlop='" + tenlop + "'";
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection(ConnectDB.getconnect());
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            da.Fill(dt);
+            con.Close();
+            da.Dispose();
+            return dt;
+        }
+        public void ThemCTGD(string tenlop, string tengv, string tenmon, string ngayday, int tiet)
+        {
+            string sql = "ThemCTGD";
+            SqlConnection con = new SqlConnection(ConnectDB.getconnect());
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@tenlop", tenlop);
+            cmd.Parameters.AddWithValue("@tengv", tengv);
+            cmd.Parameters.AddWithValue("@tenmon", tenmon);
+            cmd.Parameters.AddWithValue("@ngayday", ngayday);
+            cmd.Parameters.AddWithValue("@tiet", tiet);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+        }
     }
 }
