ea0ed22b7965e956d5459b3beeebe93339454041
Duongtsnc1 <duongbui.hlnm@gmail.com>
2020-12-13 16:27:34 +0700

    frmDangNhap Done

--- Main/Main/frmDangNhap.cs
+++ Main/Main/frmDangNhap.cs
@@ -16,5 +16,32 @@ namespace Main
         {
             InitializeComponent();
         }
+        DangNhap dn = new DangNhap();
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            if (dn.DangNhapHT(txtUserName.Text, txtPass.Text) == true)
+            {
+                // bool x = false;
+                MessageBox.Show("Bạn đăng nhập thành công ^^", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Form1 frm1 = new Form1();
+                frm1.Show();
+                Hide();
+            }
+            else MessageBox.Show("Tên đăng nhập hoặc mật khẩu sai. Mời bạn nhập lại !!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Ban có chắc muốn thoát ??", "Question", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                this.Close();
+        }
+
+        private void frmDangNhap_Load(object sender, EventArgs e)
+        {
+            SetStyle(ControlStyles.ResizeRedraw, true);
+
+        }
+
     }
 }
