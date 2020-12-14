8c58adbfe56d83d2247f9a5b74352bf74a5b7514
lelinh <lelinhmtatn@gmail.com>
2020-12-14 10:16:32 +0700

    Themsuaxoacs

--- Main/BUS/GiaoVien.cs
+++ Main/BUS/GiaoVien.cs
@@ -34,5 +34,63 @@ namespace BUS
             da.Dispose();
             return dt;
         }
+        #region Add Giaovien
+        public void ADDGiaoVien(string HoTen, string GT, string NgaySinh, string DiaChi, string SDT, string Luong, string MaMon)
+        {
+            string sql = "ADD_GV";
+            SqlConnection con = new SqlConnection(ConnectDB.getconnect());
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@HoTen", HoTen);
+            cmd.Parameters.AddWithValue("@GT", GT);
+            cmd.Parameters.AddWithValue("@NgaySinh", DateTime.Parse(NgaySinh));
+            cmd.Parameters.AddWithValue("@DiaChi", DiaChi);
+            cmd.Parameters.AddWithValue("@SDT", int.Parse(SDT));
+            cmd.Parameters.AddWithValue("@Luong", int.Parse(Luong));
+            cmd.Parameters.AddWithValue("@MaMon", MaMon);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+        }
+        #endregion
+        #region Sua Giaovien
+        public void Sua_GV(string MaGV, string HoTen, string GT, string NgaySinh, string DiaChi, string SDT, string Luong, string Mon)
+        {
+            string sql = "Sua_GV";
+            SqlConnection con = new SqlConnection(ConnectDB.getconnect());
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@MaGV", MaGV);
+            cmd.Parameters.AddWithValue("@HoTen", HoTen);
+            cmd.Parameters.AddWithValue("@GT", GT);
+            cmd.Parameters.AddWithValue("@NgaySinh", DateTime.Parse(NgaySinh));
+            cmd.Parameters.AddWithValue("@DiaChi", DiaChi);
+            cmd.Parameters.AddWithValue("@SDT", int.Parse(SDT));
+            cmd.Parameters.AddWithValue("@Luong", int.Parse(Luong));
+            cmd.Parameters.AddWithValue("@MaMon", Mon);
+
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+        }
+        #endregion
+        #region Xoa Giaovien
+        public void Xoa_GV(string MaGV)
+        {
+            string sql = "Xoa_GV";
+            SqlConnection con = new SqlConnection(ConnectDB.getconnect());
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@MaGV", MaGV);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            con.Close();
+        }
+        #endregion
     }
 }
