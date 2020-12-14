990cad55b912c284be45a379960dfad0718cae12
lelinh <lelinhmtatn@gmail.com>
2020-12-14 10:19:11 +0700

    GiaoVien

--- Main/Main/Giao_Vien.cs
+++ Main/Main/Giao_Vien.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BUS;
 
 namespace Main
 {
@@ -15,6 +16,28 @@ namespace Main
         public Giao_Vien()
         {
             InitializeComponent();
+        }
+        GiaoVien gv = new GiaoVien();
+        int chon;
+        TimKiem tk = new TimKiem();
+        public void KhoiTao()
+        {
+            txtHoTenGV.Enabled = txtLuong.Enabled = txtSDT.Enabled = cbGTGV.Enabled = cbMonHoc.Enabled = txtDiaChi.Enabled = false;
+            dtpNgaySinhGV.Enabled = false;
+            btnSua.Enabled = btnThem.Enabled = btnXoa.Enabled = true;
+            btnLuu.Enabled = false;
+        }
+        public void Mo()
+        {
+            txtHoTenGV.Enabled = txtLuong.Enabled = txtSDT.Enabled = cbGTGV.Enabled = cbMonHoc.Enabled = txtDiaChi.Enabled = true;
+            dtpNgaySinhGV.Enabled = true;
+            btnSua.Enabled = btnThem.Enabled = btnXoa.Enabled = false;
+            btnLuu.Enabled = true;
+        }
+        public void SetNull()
+        {
+            txtMaGV.Text = txtHoTenGV.Text = txtDiaChi.Text = txtLuong.Text = cbGTGV.Text = cbMonHoc.Text = cbTKGV.Text = txtTKGV.Text = txtSDT.Text = "";
+            dtpNgaySinhGV.Text = DateTime.Now.ToShortDateString();
         }
     }
 }
