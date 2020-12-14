53fadf94e531a472bbed98ff4f443beb95b775b7
lelinh <lelinhmtatn@gmail.com>
2020-12-14 10:12:20 +0700

    showgiaovien

--- Main/BUS/GiaoVien.cs
+++ Main/BUS/GiaoVien.cs
@@ -10,6 +10,29 @@ namespace BUS
 {
     public class GiaoVien
     {
-       
+        public DataTable Show()
+        {
+            string sql = "SELECT gv.MaGV, gv.HoTen, gv.GT, gv.NgaySinh, gv.DiaChi, gv.Luong, gv.SDT, mh.TenMon FROM tblGiaoVien gv, tblMonHoc mh where gv.MaMon = mh.MaMon";
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection(ConnectDB.getconnect());
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            da.Fill(dt);
+            con.Close();
+            da.Dispose();
+            return dt;
+        }
+        public DataTable Show(string tenmon)
+        {
+            string sql = "SELECT gv.MaGV, gv.HoTen FROM tblGiaoVien gv, tblMonHoc mh where gv.MaMon = mh.MaMon and mh.TenMon=N'" + tenmon + "'";
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection(ConnectDB.getconnect());
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            da.Fill(dt);
+            con.Close();
+            da.Dispose();
+            return dt;
+        }
     }
 }
