d2f925f7237a6a9b0e841050877cd78cc4032e51
ThanhThuy2908 <Thanhthuybt2908@gmail.com>
2020-12-14 10:22:58 +0700

    HuongDan1

--- Main/Main/frmHuongDan.cs
+++ Main/Main/frmHuongDan.cs
@@ -16,5 +16,52 @@ namespace Main
         {
             InitializeComponent();
         }
+
+        private void GetFileAll(string tenfile)
+        {
+            StreamReader doc = File.OpenText(tenfile);
+            string s = doc.ReadToEnd();
+            txtGioiThieu.Text = s;
+        }
+
+        private void trViewGioiThieu_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Name == "gtPhanMem")
+            {
+                GetFileAll("GioiThieuChung.txt");
+                Image img = Image.FromFile(@"truong.jpg");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtDangNhap")
+            {
+                GetFileAll("PhanDangNhap.txt");
+                Image img = Image.FromFile(@"b2 dang nhap.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtManHinhChinh")
+            {
+                GetFileAll("PhanMain.txt");
+                Image img = Image.FromFile(@"b2 main.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtGiaoVien")
+            {
+                GetFileAll("PhanQuanLyGiaoVien.txt");
+                Image img = Image.FromFile(@"b2 giao vien.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else  if (e.Node.Name == "gtHocSinh")
+            {
+                GetFileAll("PhanQuanLyHocSinh.txt");
+                Image img = Image.FromFile(@"b2 hoc sinh.png");
+                pictureBox1.BackgroundImage = img;
+            }
+            else if (e.Node.Name == "gtQLGD")
+            {
+                GetFileAll("PhanQuanLyQLGD.txt");
+                Image img = Image.FromFile(@"b2 ttgd.png");
+                pictureBox1.BackgroundImage = img;
+            }
+        }
     }
 }
